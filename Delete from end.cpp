a4f77b14e0e4e10b3af2f1cdbe1b5ad956eb0b60
Muhtasim Ahmed <muhtasimlabib289@gmail.com>
2025-01-13 23:11:24 +0600

    Delete an element from the end of an array.

---
+++ Delete from end.cpp
@@ -0,0 +1,27 @@
+/* Delete array last position
+*/
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int i,n,value,posi;
+     cout<<"Enter the rang of array\n";
+   cin>>n;
+   int a[n+1];
+    cout<<"Enter the elements of array\n";
+    for (i=0;i<n;i++)
+    {
+        cin>>a[i];
+
+    }
+    n--;
+     cout<<"After deletion at the end\n";
+       for (i=0;i<n;i++)
+       {
+           cout <<a[i]<<" ";
+
+       }
+       return 0;
+}
