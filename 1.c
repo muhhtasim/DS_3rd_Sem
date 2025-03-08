33b21079a8b66be60ea53ab5a919ef3a57cc1035
Muhtasim Ahmed <muhtasimlabib289@gmail.com>
2025-03-09 01:41:36 +0600

    Implementation of Singly Linked List

---
+++ 1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void insert(struct Node** head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = *head;
+    *head = newNode;
+}
+
+void display(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+    insert(&head, 3);
+    insert(&head, 2);
+    insert(&head, 1);
+    display(head);
+    return 0;
+}
+
